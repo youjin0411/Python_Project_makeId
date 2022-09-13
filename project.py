7441627f7b697068c428a251f34bc5e7052e5255
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-09-14 00:01:26 +0900

    숫자 암호 생성 완성

--- project.py
+++ project.py
@@ -16,34 +16,69 @@ import pandas as pd
 from pandas import read_excel
 import random
 
-ID = input("이름을 입력해주세요 : ")
 #엑셀 파일 읽기 
 data = pd.read_excel('F:\python\실습\삐삐는아이디\db.xlsx')
 li = []
 passli = []
 
-#엑셀 파일의 숫자 암호 리스트li에 담기
-for i in range(len(data['암호'])):
-    li.append(data['암호'][i])
+while True:
+    print("🖤🤍"*15)
+    print()
+    print("1번. 삐삐는 아이디 생성")
+    print("2번. 삐삐 숫자 암호 의미")
+    print("3번. 나만의 숫자 암호 만들기")
+    print("4번. 재미있는 정보")
+    print()
+    print("🖤🤍"*15)
+    print()
 
-#엑셀 파일의 암호 의미 리스트 passli에 담기
-for i in range(len(data['의미'])):
-    passli.append(data['의미'][i])
+    choi = int(input("숫자를 선택해주세요 (ex. 1 / 2 ...): "))
 
-#리스트 li안에 있는 숫자 암호 랜덤으로 하나 makeId에 주기
-makeId = random.sample(li, 1)
-ind = li.index(makeId)
+    if choi == 1:
+        ID = input("\n이름을 입력해주세요 : ")
 
-#[] 빠져나오기 
-makeId = str(makeId).strip('[]')
+        #엑셀 파일의 숫자 암호 리스트li에 담기
+        for i in range(len(data['암호'])):
+            li.append(data['암호'][i])
 
-#생성된 아이디 출력
-print("\n생성된 아이디 : ",ID,"_",makeId, "\n")
-#의미 보여주기 
-id_mean = str(passli[ind]).strip('[]')
-print("아이디 의미 : " , id_mean)
+        #엑셀 파일의 암호 의미 리스트 passli에 담기
+        for i in range(len(data['의미'])):
+            passli.append(data['의미'][i])
 
-#삐삐 숫자 암호 의미
-# print(data)
-# data['암호']
+        #리스트 li안에 있는 숫자 암호 랜덤으로 하나 makeId에 주기
+        makeId = random.sample(li, 1)
+        ind = li.index(makeId)
 
+        #[] 빠져나오기 
+        makeId = str(makeId).strip('[]')
+
+        #생성된 아이디 출력
+        print("\n생성된 아이디 : ",ID,"_",makeId, "\n")
+        #의미 보여주기 
+        id_mean = str(passli[ind]).strip('[]')
+        print("아이디 의미 : " , ID ,"_", id_mean)
+
+        print()
+        print("-"*30)
+        print("나가기 == 0 ")
+        print("선택하기 == 1")
+        print("-"*30)
+
+        res = int(input("선택해주세요 : "))
+        if res == 0:
+            exit()
+        elif res == 1:
+            print("다시 선택")
+        else :
+            print("다시 입력하세요")
+            res = int(input("선택해주세요 : "))
+        break
+    if choi == 2:
+        print(data)
+        break
+    if choi == 3:
+        print("나만의 숫자 암호 만들기")
+        break
+    if choi == 4:
+        print("재미있는 정보")
+        break
